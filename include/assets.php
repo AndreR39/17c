6a6b509a720058894566312811bf355213ddde30
dmfmmartins <42061608+dmfmmartins@users.noreply.github.com>
2019-02-28 11:17:56 +0000

    Add files via upload

---
+++ include/assets.php
@@ -0,0 +1,15 @@
+<meta charset="utf-8"> <!-- Tipo de caracteres -->
+<meta http-equiv="X-UA-Compatible" content="ie=edge"> <!-- Compatiblidade com o internet explorer -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Tamanho para diferentes dispositivos -->
+
+<link rel="shortcut icon" href="assets/img/favicon/favicon.ico"> <!-- Icon -->
+
+<link type="text/css" rel="stylesheet" href="assets/css/bootstrap.css"> <!-- Folha de estilos Bootstrap (estilos predefinidos) -->
+<link rel="stylesheet" href="assets/css/style.css"> <!-- Folha de estilos customizada -->
+
+<script type="text/javascript" src="assets/js/jquery.min.js"></script> <!-- jQuery -->
+<script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script> <!-- scripts para o Bootstrap funcionar corretamente -->
+
+<?php
+	include_once("php/conn.php"); // Ficheiro de conexão à bd
+?>
---
+++ include/footer.php
@@ -0,0 +1,5 @@
+<!-- Rodapé -->
+<div id="footer" class="jumbotron text-center mb-0">
+	<h5>Isto é um rodapé</h5>
+	<h6>12C Turno 1</h6>
+</div>
