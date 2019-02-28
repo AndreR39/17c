127c65cbe397a2924c98f8299cab4b7f3f7fa7ce
MrLuisHenrique <45965709+MrLuisHenrique@users.noreply.github.com>
2019-02-28 12:52:25 +0000

    Add files via upload

---
+++ chatroom_pub.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt" dir="ltr">
+	<head>
+		<?php require_once("include/assets.php"); ?>
+		<title>Título da Página</title>
+	</head>
+	<body>
+		<div id="wrap">
+			<?php require_once("include/header.php"); ?>
+
+			<div class="container">
+				<div class="row">
+					<div class="col-3">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas a enim ac ornare. Donec imperdiet neque molestie ante egestas sagittis. Cras sed pharetra purus, eu bibendum ligula. Suspendisse rutrum tortor eget pulvinar blandit. Donec eget metus nec tortor pretium condimentum in vitae magna. Vivamus eu lectus erat. Sed semper sapien vitae aliquet mollis. Phasellus vel metus eget nulla volutpat cursus vel venenatis magna. Vestibulum finibus semper suscipit. Maecenas tortor ipsum, gravida ac consectetur nec, sollicitudin eget nisl.</p>
+					</div>
+
+					<div class="col-9">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas a enim ac ornare. Donec imperdiet neque molestie ante egestas sagittis. Cras sed pharetra purus, eu bibendum ligula. Suspendisse rutrum tortor eget pulvinar blandit. Donec eget metus nec tortor pretium condimentum in vitae magna. Vivamus eu lectus erat. Sed semper sapien vitae aliquet mollis. Phasellus vel metus eget nulla volutpat cursus vel venenatis magna. Vestibulum finibus semper suscipit. Maecenas tortor ipsum, gravida ac consectetur nec, sollicitudin eget nisl.</p>
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<?php
+			require_once("include/footer.php");
+		?>
+	</body>
+</html>
